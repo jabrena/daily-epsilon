7b2cfdc6a3c68f04bfa2b5add110f29ef2420b40
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-01-13 11:20:54 +0000

    Minor change

--- src/main/java/edu/jab/math/Problem20220113.java
+++ src/main/java/edu/jab/math/Problem20220113.java
@@ -4,15 +4,15 @@
 
 package edu.jab.math;
 
-import org.apache.commons.math3.util.FastMath;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.commons.math3.util.FastMath;
 
 public class Problem20220113 {
 
     public static void main(String[] args) {
 
-        //https://en.m.wikipedia.org/wiki/Binary_logarithm
+        // https://en.m.wikipedia.org/wiki/Binary_logarithm
         var result = FastMath.log(2, 5000);
         var solution = Math.ceil(result);
 
