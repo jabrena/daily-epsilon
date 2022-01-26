7611720a64238a20d00f39a4fef2a679f2eed573
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-01-26 14:50:29 +0100

    Refactoring to not reinvent the wheel to calculate prime Numbers

--- src/main/java/edu/jab/math/Problem20220123.java
+++ src/main/java/edu/jab/math/Problem20220123.java
@@ -1,5 +1,6 @@
 /// usr/bin/env jbang "$0" "$@" ; exit $?
 // DEPS org.assertj:assertj-core:3.21.0
+// DEPS org.apache.commons:commons-math3:3.6.1
 
 package edu.jab.math;
 
@@ -7,22 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 import java.util.stream.Stream;
+import org.apache.commons.math3.primes.Primes;
 
 public class Problem20220123 {
 
     public static void main(String[] args) {
 
-        Predicate<Integer> isPrime =
-                number -> {
-                    if (number == 2) {
-                        return true;
-                    } else if (number == 1 || number % 2 == 0) {
-                        return false;
-                    }
-                    return LongStream.rangeClosed(2, number / 2).noneMatch(i -> number % i == 0);
-                };
+        Predicate<Integer> isPrime = number -> Primes.isPrime(number);
 
         Predicate<Integer> checkPrimeDigits =
                 number -> {
