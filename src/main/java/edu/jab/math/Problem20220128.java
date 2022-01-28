f6072fd703f16c3f17b2b237add8c00e362a183f
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-01-28 16:31:19 +0100

    Improving solution

--- src/main/java/edu/jab/math/Problem20220128.java
+++ src/main/java/edu/jab/math/Problem20220128.java
@@ -3,37 +3,17 @@
 
 package edu.jab.math;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.util.function.Predicate;
-import java.util.stream.LongStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.matheclipse.core.eval.EvalUtilities;
 
 public class Problem20220128 {
 
     public static void main(String[] args) {
 
-        //TODO: Does exist a library to express MathML into Java?
-        Predicate<Long> checkEquation = x -> {
-            var precision = new MathContext(20);
-            var component1LeftPart = BigDecimal.ONE.divide(BigDecimal.valueOf(4), precision).multiply(BigDecimal.valueOf(x));
-            var component2LeftPart = BigDecimal.ONE.divide(BigDecimal.valueOf(7), precision).multiply(BigDecimal.valueOf(x));
-            var leftPart = component1LeftPart.multiply(component2LeftPart);
-            var leftPartRounded = leftPart.setScale(0, RoundingMode.HALF_UP);
-            var rightPart = BigDecimal.valueOf(x);
+        var util = new EvalUtilities(false, true);
 
-            return (leftPartRounded.compareTo(rightPart) == 0) ? true : false;
-        };
-
-        var solution = LongStream.rangeClosed(1, 1000).boxed()
-                        .filter(checkEquation)
-                        .limit(1)
-                        .findFirst()
-                        .orElseThrow();
+        var equation = "Solve({(x/4)*(x/7)==x, x>0}, {x})";
+        var solution = util.evaluate(equation);
 
         System.out.println(solution);
-        assertThat(solution).isEqualTo(28);
     }
 }
